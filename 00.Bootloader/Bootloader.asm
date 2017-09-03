47945ddaca3ae312b845901659ef59fa9980e862
pr0v3rbs <rla5072@paran.com>
2017-09-03 18:04:18 +0900

    Update bootloader
    Add loading image code from disk
    Add print function

--- 00.Bootloader/Bootloader.asm
+++ 00.Bootloader/Bootloader.asm
@@ -5,35 +5,163 @@ SECTION .text
 
 jmp 0x07C0:START
 
-START:
-mov ax, 0x07C0
-mov ds, ax
-mov ax, 0xB800
-mov es, ax
-mov si, 0
+TOTALSECTORCOUNT: dw 1024
 
-.SCREENCLEARLOOP:
-mov byte [es:si], 0
-mov byte [es:si + 1], 0x0A
-add si, 2
-cmp si, 80 * 25 * 2
-jl .SCREENCLEARLOOP
+START:
+  ; set bootloader address
+  mov ax, 0x07C0
+  mov ds, ax
+  mov ax, 0xB800
+  mov es, ax
+  ; stack setting
+  mov ax, 0x0000
+  mov ss, ax
+  mov sp, 0xFFFE
+  mov bp, 0xFFFE
+
+
+; clear screen
 mov si, 0
-mov di, 0
+.SCREENCLEARLOOP:
+  mov byte [es:si], 0
+  mov byte [es:si + 1], 0x0A
+  add si, 2
+  cmp si, 80 * 25 * 2
+  jl .SCREENCLEARLOOP
+
+
+push MESSAGE1
+push 0
+push 0
+call PRINTMESSAGE
+add sp, 6
+
+push LOADINGMESSAGE
+push 1
+push 0
+call PRINTMESSAGE
+add sp, 6
+
+
+RESETDISK:
+  mov ax, 0
+  mov dl, 0
+  int 0x13
+  jc HANDLEDISKERROR
+  ; set destination address to load the image
+  mov si, 0x1000
+  mov es, si
+  mov bx, 0x0000 ; final destination 0x1000:0000 (0x10000)
+
+
+mov di, word [TOTALSECTORCOUNT]
+READDATA:
+  cmp di, 0
+  je READEND
+  sub di, 0x1
+
+  ; BIOS Read Function
+  mov ah, 0x02
+  mov al, 0x1
+  mov ch, byte [TRACKNUMBER]
+  mov cl, byte [SECTORNUMBER]
+  mov dh, byte [HEADNUMBER]
+  mov dl, 0x00                  ; interrupt drive number (0=Floppy)
+  int 0x13
+  jc HANDLEDISKERROR
+
+  add si, 0x0020
+  mov es, si      ; move next memory address (1 Sector == 512 bytes)
+
+  mov al, byte [SECTORNUMBER]
+  add al, 0x01
+  mov byte [SECTORNUMBER], al
+  cmp al, 19
+  jl READDATA
+
+  xor byte [HEADNUMBER], 0x01
+  mov byte [SECTORNUMBER], 0x01
+
+  cmp byte [HEADNUMBER], 0x00
+  jne READDATA
+
+  add byte [TRACKNUMBER], 0x01
+  jmp READDATA
+
+
+READEND:
+  push COMPLETEMESSAGE
+  push 1
+  push 20
+  call PRINTMESSAGE
+  add sp, 6
+
+  jmp 0x1000:0x0000
+
+
+HANDLEDISKERROR:
+  push DISKERRORMESSAGE
+  push 1
+  push 20
+  call PRINTMESSAGE
+  add sp, 6
+  jmp $
+
+
+PRINTMESSAGE:
+  push bp
+  mov bp, sp
+  push es
+  push si
+  push di
+  push ax
+  push cx
+  push dx
+
+  mov ax, 0xB800
+  mov es, ax
+
+  ; setting display address
+  mov ax, word [bp + 6]
+  mov si, 160
+  mul si
+  mov di, ax
+
+  mov ax, word [bp + 4]
+  mov si, 2
+  mul si
+  add di, ax
+
+  mov si, word [bp + 8]
 
 .MESSAGELOOP:
-mov cl, byte [si + MESSAGE1]
-cmp cl, 0
-je .MESSAGEEND
-mov byte [es:di], cl
-add si, 1
-add di, 2
-jmp .MESSAGELOOP
+  mov cl, byte [si]
+  cmp cl, 0
+  je .MESSAGEEND
+  mov byte [es:di], cl
+  add si, 1
+  add di, 2
+  jmp .MESSAGELOOP
 
 .MESSAGEEND:
-jmp $
+  pop dx
+  pop cx
+  pop ax
+  pop di
+  pop si
+  pop es
+  pop bp
+  ret
 
-MESSAGE1: db 'MINT64 OS BootLoader~!', 0
+; Data section
+MESSAGE1:   db 'MINT64 OS Boot Loader Start~!!', 0
+DISKERRORMESSAGE: db  'DISK Error~!!', 0
+LOADINGMESSAGE:   db  'OS Image Loading...', 0
+COMPLETEMESSAGE:  db  'Complete~!!', 0
+
+SECTORNUMBER:     db 0x02
+HEADNUMBER:       db 0x00
+TRACKNUMBER:      db 0x00
 
 times 510 - ($ - $$) db 0x00
 
